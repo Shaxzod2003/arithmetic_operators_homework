072d708512b65225d8e6750fba294d8fae021310
Shaxzod2003 <Shaxzod2003@Sulaymonovshaxzod77@gmail.com>
2023-06-09 11:47:49 +0500

    complete

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -1,5 +1,7 @@
 #Create a varaible 'number' and assign it the value of 43.
-
+number=43
 #Create a variable 'answer' and assign it the sum of the number's digits.
-
-#Print the vallue of the 'answer'.
+d1=number%10
+d2=number//10
+#Print the vallue of the 'ansswer'.
+print(d1+d2)
