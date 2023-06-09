9ceade99c08ccc6b3469a3b63bbc68b8899d83b5
Shaxzod2003 <Shaxzod2003@Sulaymonovshaxzod77@gmail.com>
2023-06-09 11:53:27 +0500

    complete

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,7 +1,9 @@
 #Create a variable called 'number' and assign it the two-digit number
-
+number=87
 #Find the reverse of the number and assign it to a variable called 'answer'.
-
+d1=number%10
+d2=number//10
+answer=d1*10+d2
 #Print the value of the 'answer'
-
+print(answer)
 
